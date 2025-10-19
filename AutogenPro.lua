57b390b079e127e6d8413e7735fa3a88827dc177
NgKhaiReal <giabaohua47@gmail.com>
2025-10-19 15:50:54 +0700

    Create AutogenPro.lua

---
+++ AutogenPro.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/30873cf71256e90b953698ba0a72668b658b6e84ea80131020b7500fe4d20d7b/download"))()
+
+
+
+-- Api Script + lura ph why are you try to get free src😭
