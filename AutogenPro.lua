0c72382c5bd500478d63a52a3eaba6bd638bcbd4
NgKhaiReal <giabaohua47@gmail.com>
2025-10-28 07:48:31 +0700

    Update AutogenPro.lua

--- AutogenPro.lua
+++ AutogenPro.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/17949d08ead3e4d6721074e3293a2f65b4a60b9df654f6cfdce46a8dd03bba81/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f8f46aa31252f4fa9b50a9056949e066028798b9194b0402cb68cbc2bd88912c/download"))()
 
 
 -- Why Are You Trying Deobf ? these is a api Script loll
